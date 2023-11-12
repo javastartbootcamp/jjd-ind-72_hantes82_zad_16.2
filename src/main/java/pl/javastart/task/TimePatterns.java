069d968a3a2a3ee11d7c14351b0e115a1eb53c39
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-11-12 10:57:37 +0100

    Zadanie 16.2 -mala poprawka

--- src/main/java/pl/javastart/task/TimePatterns.java
+++ src/main/java/pl/javastart/task/TimePatterns.java
@@ -8,14 +8,15 @@ import java.util.*;
 public class TimePatterns {
     public static LocalDateTime parseTimeFormat(String userInput) {
         List<String> formatters = new ArrayList<>();
-        Collections.addAll(formatters, "yyyy-MM-dd HH:mm:ss", "dd.MM.yyyy HH:mm:ss", "yyyy-MM-dd");
+        Collections.addAll(formatters, "yyyy-MM-dd HH:mm:ss", "dd.MM.yyyy HH:mm:ss");
         LocalDateTime parsedDateTime = null;
         boolean parsed = false;
+        if (!userInput.contains(" ")) {
+            userInput += " 00:00:00";
+        }
         for (String formatter : formatters) {
             try {
-                if (!userInput.contains(" ")) {
-                    userInput += " 00:00:00";
-                }
+
                 DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formatter);
                 parsedDateTime = LocalDateTime.parse(userInput, dateFormatter);
                 parsed = true;
